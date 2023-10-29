1bb56ff50857ed5ffb6e552257e265504f157937
MauroGutierrez53 <mauro.5389@gmail.com>
2023-10-29 12:04:27 -0300

    se crea logo de pagina

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
