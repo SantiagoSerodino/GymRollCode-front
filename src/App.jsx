597df7f277b5ef867ff3046ae0886b56b1f4277b
santi0dino <serodinosantiago765@gmail.com>
2023-10-31 02:15:07 -0300

    se creo base del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './pages/HomeScreen';
+import Login from './pages/loginPage';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
      <Routes>
       <Route path='/' element={<HomeScreen/>}/>
+      <Route path='/login' element={<Login/>}/>
      </Routes>
     </BrowserRouter>
   )
