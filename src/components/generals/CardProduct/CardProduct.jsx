b4b403e46db3825e7865dc020cfe9c4fe9e24d3c
Naty <natyaguirre24@gmail.com>
2023-10-31 01:03:43 -0300

    realize el archivo HomePage

--- src/components/generals/CardProduct/CardProduct.jsx
+++ src/components/generals/CardProduct/CardProduct.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const CardProduct = () => {
+const CardProduct = (props) => {
   return (
     <div className="card" style={{width: '18rem'}}>
+      <img src="" alt="" />
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text"></p>
