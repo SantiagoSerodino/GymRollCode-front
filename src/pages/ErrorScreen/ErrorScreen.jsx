417c0cb6e6fe398ee4b5c58872693a3ac369591c
MauroGutierrez53 <mauro.5389@gmail.com>
2023-10-31 21:21:32 -0300

    se completo pagina de error404

--- src/pages/ErrorScreen/ErrorScreen.jsx
+++ src/pages/ErrorScreen/ErrorScreen.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import '../ErrorScreen/StyleErrorScreen.css'
 
+
 const ErrorScreen = () => {
 
    return (
       <>
-      <body className='bodyErrorMG '>
-      <div>ErrorScreen</div>
-      <header ClassName='container text-center py-4'>
-         <img src='./src/assets/logoPagina2.png' alt='Logo de la página' ClassName='img-fluid logoImgMG' />
-      </header>
-         <div ClassName='container text-center py-5'>
-            <img src='./src/assets/404.png' alt='Error 404' ClassName='img-fluid errorImgMG' />
+         <div className="container-fluid bodyErrorMG">
+            <div className="row justify-content-center">
+               <div className="col-12 col-md-6 col-lg-4">
+                  <header className="text-center pt-2">
+                  <img src="./src/assets/logoPagina2.png" alt="Logo de la página" className="img-fluid py-5 py-md-1 w-50" />
+                  </header>
+                  <div className="text-center py-4 ">
+                  <img src="./src/assets/404.png" alt="Error 404" className="img-fluid w-100" />
+                  </div>
+                  <div className="text-center pb-2">
+                  <h1 className=" text-uppercase fs-4 p-4">No se pudo conectar</h1>
+                  <a href="/" className="btn  text-light bg-dark  my-2">HOME</a>
+                  </div>
+                  <div className=" container text-center pt-2">
+                     <div className="row  copyrightMG">
+                        <div className="col">
+                           <p>© 2023 GymRoll</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
-         <div ClassName='container text-center pb-5'>
-            <h1 ClassName='numero404MG '>no se puedo conectar</h1>
-            <a href='/' ClassName='btn btn-primary volverBtnMG'>Volver a la página principal</a>
-         </div>
-         <footer ClassName=' container text-center py-4'>
-            <p ClassName='copyrightMG'>© 2023 Nombre de la Página | Disculpa las molestias</p>
-         </footer>
-         
-      </body>
-      
       </>
    )
 }
