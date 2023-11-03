630e03295fb068d571e0fc381a6aee91c5eec070
leandro cudmani <leandro.cudmani.tucu@gmail.com>
2023-11-03 16:45:57 -0300

    Agregue validaciones en el registro y corregi algunos errores

--- src/Components/Specific/Register/RegisterForm.jsx
+++ src/Components/Specific/Register/RegisterForm.jsx
@@ -23,8 +23,34 @@ const RegisterForm = () => {
      
   };
 
-  const handleSubmit = () => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const nameRegex = /^[A-Za-z]{3,12}$/;
+    const lastNameRegex = /^[A-Za-z]{3,12}$/;
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    const passwordRegex = /^.{8,}$/;
+
+    if (!nameRegex.test(registerForm.name)) {
+      alert('El campo debe tener entre 3 y 12 carecteres');
+      return;
+    }
+
+    if (!lastNameRegex.test(registerForm.lastName)) {
+      alert('El campo debe tener entre 3 y 12 carecteres');
+      return;
+    }
+
+    if (!emailRegex.test(registerForm.email)) {
+      alert('El correo electronico no es valido');
+      return;
+    }
+
+    if (!passwordRegex.test(registerForm.password)) {
+      alert('La contraseña debe tener al menos 8 caracteres');
+      return;
+    }
+
+
     axios.post('/register', registerForm)
     .then((response) => console.log(response))
     .catch((error) => console.log(error))
