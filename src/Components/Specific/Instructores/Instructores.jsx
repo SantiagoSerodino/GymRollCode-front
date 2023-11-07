b4b8b554ff66b3490771ba6fc2b928af3f343593
MauroGutierrez53 <mauro.5389@gmail.com>
2023-11-06 23:16:11 -0300

    instructores

--- src/Components/Specific/Instructores/Instructores.jsx
+++ src/Components/Specific/Instructores/Instructores.jsx
@@ -4,47 +4,35 @@ import '../Instructores/StyleInstructores.css'
 const Instructores = () => {
     return (
         <>
-        <h1>
-            Instructores
+        <div className='instructoresMG pb-5'>
+        <h1 className='  py-4' >
+            Nuestros Instructores 
         </h1>
-        {/* <div className='row'>
-        <section className='col-12 justify-content-center sectionInstrucMG'>
-            <img className='imgMG' src="../src/assets/Instructores/instructor1.jpg" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor3.webp" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor2.jpg" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor1.jpg" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor3.webp" alt="" />
-        </section>
-        </div> */}
-
         <div className='row'>
         <section className='col-12 justify-content-center sectionInstrucMG'>
         <div className=" imgMG">
-            <img src="../src/assets/Instructores/instructor3.webp" className="imagenn " alt="..."/>
-            <h5 className="card-img-overlay">Card title</h5>
+            <img src="../src/assets/Instructores/instructor4.jpg" className="imagenn " alt="..."/>
+            <h5 className="card-img-overlay ">Mario Baracus </h5>
         </div>
         <div className=" imgMG">
-            <img src="../src/assets/Instructores/instructor2.jpg" className="imagenn " alt="..."/>
-            <h5 className="card-img-overlay">Card title</h5>
+            <img src="../src/assets/Instructores/instructor5.jpeg" className="imagenn " alt="..."/>
+            <h5 className="card-img-overlay">Caterine Fulo</h5>
         </div>
         <div className=" imgMG">
             <img src="../src/assets/Instructores/instructor1.jpg" className="imagenn " alt="..."/>
-            <h5 className="card-img-overlay">Card title</h5>
+            <h5 className="card-img-overlay">Rocky Balboa</h5>
         </div>
         <div className=" imgMG">
             <img src="../src/assets/Instructores/instructor3.webp" className="imagenn " alt="..."/>
-            <h5 className="card-img-overlay">Card title</h5>
+            <h5 className="card-img-overlay">Ronda Rousey</h5>
         </div>
         <div className=" imgMG">
             <img src="../src/assets/Instructores/instructor2.jpg" className="imagenn " alt="..."/>
-            <h5 className="card-img-overlay">Card title</h5>
+            <h5 className="card-img-overlay">The Rock Johnson</h5>
         </div>
 
-            {/* <img className='imgMG' src="../src/assets/Instructores/instructor3.webp" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor2.jpg" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor1.jpg" alt="" />
-            <img className='imgMG' src="../src/assets/Instructores/instructor3.webp" alt="" /> */}
         </section>
+        </div>
         </div>
 
 
