7314675c1b114d4b8c06a8972ef57007e8645bd2
santi0dino <serodinosantiago765@gmail.com>
2023-11-07 17:08:09 -0300

    archivos borrados

--- src/Components/Specific/Login/loginPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-import SubmitButton from '../../generals/SubmitButton/SubmitButton';
-import InputForm from '../../generals/InputForm/InputForm';
