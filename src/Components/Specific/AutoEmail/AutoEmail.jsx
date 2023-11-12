995a19415c9872ce0e92cbebad4480f1724bf77a
MauroGutierrez53 <mauro.5389@gmail.com>
2023-11-12 19:59:04 -0300

    formulario funcionando

--- src/Components/Specific/AutoEmail/AutoEmail.jsx
+++ src/Components/Specific/AutoEmail/AutoEmail.jsx
@@ -1,49 +1,64 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Form from 'react-bootstrap/Form';
 import '../AutoEmail/StyleAutoEmail.css'
-
-
 
 const AutoEmail = () => {
 
+  const form = useRef();
 
-  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_2a53ebs', 'template_fdqb0ss', form.current, 'Kr4FcE7jz6B_tQ9Bx')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
 
 
   return (
-
     <>
-    
-    <h1>consulta por tus planes</h1>
-
-    <div className="container contenedorAutoEmailMG">
-      <div className="row">
-        <form>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" classNameName="form-label">Nombre y Apellido</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+    <div className="container  w-50 contenedorAutoEmailMG">
+      <div className="row fs-2 justify-content-between ">
+      <h1 className=''>consulta por tus planes</h1>
+        <form className=' formularioMG' ref={form} onSubmit={sendEmail}>
+          <div className="mb-3 d-flex justify-content-around">
+            <label  className=" tituloMG form-label w-50">Nombre: </label>
+            <input className='campoMG w-50' type="text" name="name" />
           </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"/>
+          <div className="mb-3 d-flex justify-content-around">
+            <label  className=" tituloMG form-label">Apellido:</label>
+            <input className='campoMG w-50' type="text" name="last" />
           </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" for="exampleCheck1">Check me out</label>
+          <div className="mb-3 d-flex justify-content-around">
+            <label  className=" tituloMG form-label">Celular / Telfono:</label>
+            <input className='campoMG w-50' type="number" name="phone" />
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <div className="mb-3 d-flex justify-content-around">
+            <label className=" tituloMG form-label">Email:</label>
+            <input className='campoMG w-50' type="email" name="email" />
+          </div>
+          <div className=' d-flex justify-content-around'>
+            <label className=" tituloMG form-label w-50">Planes disponibles:</label>
+            <Form.Select aria-label="Default select example" className='campoMG w-50' name="opcion">
+              <option className=''>Seleciona el tipode plan</option>
+              <option value="MUSCULACION">MUSCULACION</option>
+              <option value="CLASES">CLASES</option>
+              <option value="CLASES">FULL</option>
+            </Form.Select>
+          </div>
+          <input className="btn btn-primary" type="submit" value="Send" />
         </form>
-
       </div>
-
     </div>
 
+      </>
+  );
+};
 
-
-
-    </>
-
-  )
-}
-
-export default AutoEmail
+export default AutoEmail;
