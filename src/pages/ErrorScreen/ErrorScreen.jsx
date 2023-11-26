04668aef98458648930080c58b32ed45003ea353
MauroGutierrez53 <mauro.5389@gmail.com>
2023-11-26 10:10:28 -0300

    se resolvio unconvenientes en link de planes card

--- src/pages/ErrorScreen/ErrorScreen.jsx
+++ src/pages/ErrorScreen/ErrorScreen.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import '../ErrorScreen/StyleErrorScreen.css'
-import logoPaginaError from '../../assets/logoPagina2.png'
-import logoError from '../../assets/404.png'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../ErrorScreen/StyleErrorScreen.css';
+import logoPaginaError from '../../assets/logoPagina2.png';
+import logoError from '../../assets/404.png';
 
 const ErrorScreen = () => {
-
    return (
       <>
          <div className='container-fluid bodyErrorMG'>
             <div className='row justify-content-center'>
                <div className='col-12 col-md-6 col-lg-4'>
                   <div className='text-center pt-2'>
-                  <img src={logoPaginaError} alt='Logo de la página' className='img-fluid py-5 py-md-1 w-50' />
+                     <img src={logoPaginaError} alt='Logo de la página' className='img-fluid py-5 py-md-1 w-50' />
                   </div>
                   <div className='text-center py-4 '>
-                  <img src={logoError} alt='Error 404' className='img-fluid w-100' />
+                     <img src={logoError} alt='Error 404' className='img-fluid w-100' />
                   </div>
                   <div className='text-center pb-2'>
-                  <h1 className=' text-uppercase fs-4 p-4'>No se pudo conectar</h1>
-                  <a href='/' className='btn  text-light bg-dark my-2'>Inicio</a>
+                     <h1 className=' text-uppercase fs-4 p-4'>No se pudo conectar</h1>
+                     <Link to='/' className='btn text-light bg-dark my-2'>
+                        Inicio
+                     </Link>
                   </div>
                   <div className='container text-center pt-2'>
                      <div className='row  copyrightMG'>
@@ -32,7 +33,7 @@ const ErrorScreen = () => {
             </div>
          </div>
       </>
-   )
-}
+   );
+};
 
-export default ErrorScreen
+export default ErrorScreen;
