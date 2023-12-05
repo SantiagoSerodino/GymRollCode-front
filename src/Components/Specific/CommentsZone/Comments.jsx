2a5157dbb7c8af6bcc9bef7d365cf4fd682d8c69
MauroGutierrez53 <mauro.5389@gmail.com>
2023-12-05 19:58:05 -0300

    se modifica zona comentario para que el mismo quede fijado luego de recargar la pagina

--- src/Components/Specific/CommentsZone/Comments.jsx
+++ src/Components/Specific/CommentsZone/Comments.jsx
@@ -1,61 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function CommentSection() {
-    const [comments, setComments] = useState([]);
+    const [comments, setComments] = useState(() => {
+        // agrega los comentarios al localstorage
+        const storedComments = localStorage.getItem('comments');
+        return storedComments ? JSON.parse(storedComments) : [];
+    });
     const [newComment, setNewComment] = useState('');
-    const [currentUser, setCurrentUser] = useState('UsuarioEjemplo'); // Simulación de usuario actual (puedes ajustarlo)
+    const [currentUser, setCurrentUser] = useState('UsuarioEjemplo');
+
+    useEffect(() => {
+        // Guardar comentarios en localStorage cuando son modificados 
+        localStorage.setItem('comments', JSON.stringify(comments));
+    }, [comments]);
 
     const handleCommentChange = (e) => {
-    setNewComment(e.target.value);
+        setNewComment(e.target.value);
     };
 
     const handleAddComment = () => {
-    setComments([...comments, { text: newComment, user: currentUser }]);
-    setNewComment('');
+        setComments([...comments, { text: newComment, user: currentUser }]);
+        setNewComment('');
     };
 
     const handleDeleteComment = (index) => {
-    const commentToDelete = comments[index];
-    if (commentToDelete.user === currentUser) {
-        const updatedComments = [...comments];
-        updatedComments.splice(index, 1);
-        setComments(updatedComments);
-    } else {
-        alert("No puedes eliminar un comentario que no creaste.");
-    }
+        const commentToDelete = comments[index];
+        if (commentToDelete.user === currentUser) {
+            const updatedComments = [...comments];
+            updatedComments.splice(index, 1);
+            setComments(updatedComments);
+        } else {
+            alert("No puedes eliminar un comentario que no creaste.");
+        }
     };
 
     return (
-    <div>
-        <h2 className='fw-bold text-decoration-underline text-success fs-1 pb-4'>Comentarios</h2>
-        <div className="mb-3">
-        <textarea
-          className="form-control"
-          rows="4"
-          value={newComment}
-          onChange={handleCommentChange}
-          placeholder="Escribe un comentario..."
-        />
-        </div>
-        <button className="btn btn-outline-light mt-3 mb-5" onClick={handleAddComment}>
-        Agregar Comentario
-        </button>
         <div>
-        {comments.map((comment, index) => (
-            <div key={index} className="alert alert-info">
-            {comment.text}
-            {comment.user === currentUser && (
-                <button
-                className="btn btn-danger btn-sm ms-2"
-                onClick={() => handleDeleteComment(index)}
-                >
-                Eliminar
-                </button>
-            )}
+            <h2 className='fw-bold text-decoration-underline text-success fs-1 pb-4'>Comentarios</h2>
+            <div className="mb-3">
+                <textarea
+                    className="form-control"
+                    rows="4"
+                    value={newComment}
+                    onChange={handleCommentChange}
+                    placeholder="Escribe un comentario..."
+                />
             </div>
-        ))}
+            <button className="btn btn-outline-light mt-3 mb-5" onClick={handleAddComment}>
+                Agregar Comentario
+            </button>
+            <div>
+                {comments.map((comment, index) => (
+                    <div key={index} className="alert alert-info">
+                        {comment.text}
+                        {comment.user === currentUser && (
+                            <button
+                                className="btn btn-danger btn-sm ms-2"
+                                onClick={() => handleDeleteComment(index)}
+                            >
+                                Eliminar
+                            </button>
+                        )}
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
     );
 }
 
